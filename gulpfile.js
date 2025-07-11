22fe2e359d0cea85cab4c0a7c386e53c931aa792
Guilherme <guilermecarvalho@live.com>
2025-07-11 17:36:43 -0300

    Criando o Hero e importando as fontes

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ function styles() {
 }
 
 function images() {
-    return gulp.src('./src/img/**/*')
+    return gulp.src('./src/img/**/*', {encoding: false})
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/img'));
 }
